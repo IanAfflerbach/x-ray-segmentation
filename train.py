69739be77dbda571233f7df6d8c666c5cf40b909
Ian Afflerbach <ianafflerbach42@gmail.com>
2025-04-25 18:01:50 -0500

    adding display of metrics

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from dataset import CXRSegDataset
 from model import SegmentationModel
 import utils
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.utils.data import DataLoader, random_split
@@ -20,8 +21,12 @@ loss = torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.05)
 scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
 
+# metric buffers
+total_losses = []
+total_acc = []
+
 # run training loop
-NUM_EPOCHS = 20
+NUM_EPOCHS = 40
 iteration = 0
 for epoch in range(NUM_EPOCHS):
     # set model to train mode
@@ -37,7 +42,7 @@ for epoch in range(NUM_EPOCHS):
         pred = model(img)
 
         # compute loss
-        l = loss(pred, label)
+        l = loss(pred, label)   
         losses.append(l.item())
         
         # compute gradiant
@@ -52,10 +57,12 @@ for epoch in range(NUM_EPOCHS):
     accs = []
     for img, label in test_loader:
         pred = model.get_mask_tensor(img)
-        accs.append((pred == label).float().mean())
+        accs.append((pred == label).float().mean().item())
 
     # epoch complete
     print("Epoch:", epoch, "... Complete, Avg Loss:", np.mean(losses), "Avg Accuracy:", np.mean(accs))
+    total_losses += losses
+    total_acc += accs
 
     # step scheduler
     scheduler.step()
@@ -64,3 +71,16 @@ for epoch in range(NUM_EPOCHS):
 MODEL_NAME = 'model.th'
 print("Model saved to", MODEL_NAME)
 utils.save_model(model, MODEL_NAME)
+
+# display loss/accuracy metrics
+plt.title('Loss over Iterations')
+plt.plot(range(len(total_losses)), total_losses)
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
+plt.show()
+
+plt.title('Validation Accuracy over Iterations')
+plt.plot(range(len(total_acc)), total_acc)
+plt.xlabel('Iteration')
+plt.ylabel('Accuracy')
+plt.show()
